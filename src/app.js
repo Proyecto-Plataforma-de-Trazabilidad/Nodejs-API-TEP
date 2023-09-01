ce5e445b9f2e2e9edcac2f16053fd5ed84b2eea8
SergioPiña <117691802+InfectedCupcake@users.noreply.github.com>
2023-08-31 21:16:08 -0600

    Ya se programo la tarea

--- src/app.js
+++ src/app.js
@@ -19,90 +19,92 @@ const app = express();
 
 
 //Cron Jobs (tareas automatizadas)
-// let task = cron.schedule("0 8 * * 0-7", () => {
-//     //Actualizar el nivel de las alertas
-//     Tareas.obtenerAlerta().then(alertas => {
+let task = cron.schedule("0 8 * * 0-7", () => { //La tarea se va a ejecutar: A las 08:00 todos los días de la semana de domingo a domingo.
+    const hoy = new Date().toLocaleString();
+    console.log("Ejecutando tarea calendarizada el: " + hoy);
 
-//         alertas.map(alerta => {
+    //Fetch Cat API
+    axios.get('https://api.thecatapi.com/v1/images/search')
+        .then(response => {
 
-//             if (alerta.Semanas == 2 && alerta.NivelFecha == 'Verde') {
-//                 console.log("Sube a nivel amarillo:" + alerta.IdAlerta);
-
-//                 Tareas.actualizarNivel(alerta.IdAlerta, 'Amarillo').then(res => { //Función que actualiza el nivel de la alerta
-
-//                     if (res == 'todoCorrecto') {
-//                         axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
-//                             opcion: "PorTipoUsuario",
-//                             parametros: 'Amocali',
-//                             titulo: "Alerta de recolección",
-//                             cuerpo: `La alerta ${alerta.IdAlerta} subió a nivel Amarillo`,
-//                             url: "https://campolimpiojal.com/Alertas/alertaGeneradasDist.php"
-//                         })
-//                             .then(response => {
-//                                 console.log(response.data);
-//                             })
-//                             .catch(error => { console.log(error); });
-//                     } //Fin del if
-
-//                 });//Fin de la funcion actualiza
-
-//             } else if (alerta.Semanas > 3 && alerta.NivelFecha == 'Amarillo') {
-//                 console.log("Sube a nivel rojo:" + alerta.IdAlerta);
-
-//                 Tareas.actualizarNivel(alerta.IdAlerta, 'Rojo').then(res => { //Función que actualiza el nivel de la alerta
-
-//                     if (res == 'todoCorrecto') {
-//                         axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
-//                             opcion: "PorTipoUsuario",
-//                             parametros: 'Amocali',
-//                             titulo: "Alerta de recolección",
-//                             cuerpo: `La alerta ${alerta.IdAlerta} subió a nivel Rojo`,
-//                             url: "https://campolimpiojal.com/Alertas/alertaGeneradasDist.php"
-//                         })
-//                             .then(response => {
-//                                 console.log(response.data);
-//                             })
-//                             .catch(error => { console.log(error); });
-//                     } //Fin del if
-
-//                 });//Fin de la funcion actualiza
-//             }
-
-//         });
-
-//     });
-
-
-// });
-
-// Tareas.comprobarAlerta().then(datos => {
-//     console.log(datos.NoAlertas);
-//     if (datos.NoAlertas > 0)
-//         task.start();
-
-// })
-
-//Fetch Cat API
-axios.get('https://api.thecatapi.com/v1/images/search')
-    .then(response => {
-
-        //Envia saludo
-        axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
-            opcion: "PorTipoUsuario",
-            parametros: 'Amocali',
-            titulo: "Buenos Dias",
-            cuerpo: `Ya levántate HUEVON`,
-            url: "https://campolimpiojal.com/",
-            imagen: response.data[0].url
-        })
-            .then(res => {
-                console.log(res.data);
+            //Envia saludo
+            axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
+                opcion: "PorTipoUsuario",
+                parametros: 'Amocali',
+                titulo: "Buenos Dias",
+                cuerpo: `Ya levántate HUEVON!!!`,
+                url: "https://campolimpiojal.com/",
+                imagen: response.data[0].url
             })
-            .catch(error => { console.log(error); });
+                .then(res => {
+                    console.log(res.data);
+                })
+                .catch(error => { console.log(error); });
 
-    })
-    .catch(error => { console.log(error); });
+        })
+        .catch(error => { console.log(error); });
 
+    //Actualizar el nivel de las alertas
+    Tareas.obtenerAlerta().then(alertas => {
+
+        alertas.map(alerta => {
+
+            if (alerta.Semanas == 2 && alerta.NivelFecha == 'Verde') {
+                console.log("Sube a nivel amarillo:" + alerta.IdAlerta);
+
+                Tareas.actualizarNivel(alerta.IdAlerta, 'Amarillo').then(res => { //Función que actualiza el nivel de la alerta
+
+                    if (res == 'todoCorrecto') {
+                        axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
+                            opcion: "PorTipoUsuario",
+                            parametros: 'Amocali',
+                            titulo: "Alerta de recolección",
+                            cuerpo: `La alerta ${alerta.IdAlerta} subió a nivel Amarillo`,
+                            url: "https://campolimpiojal.com/Alertas/alertaGeneradasDist.php"
+                        })
+                            .then(response => {
+                                console.log(response.data);
+                            })
+                            .catch(error => { console.log(error); });
+                    } //Fin del if
+
+                });//Fin de la funcion actualiza
+
+            } else if (alerta.Semanas > 3 && alerta.NivelFecha == 'Amarillo') {
+                console.log("Sube a nivel rojo:" + alerta.IdAlerta);
+
+                Tareas.actualizarNivel(alerta.IdAlerta, 'Rojo').then(res => { //Función que actualiza el nivel de la alerta
+
+                    if (res == 'todoCorrecto') {
+                        axios.post('https://nodejs-api-tep-production.up.railway.app/serviciopush/enviar', {
+                            opcion: "PorTipoUsuario",
+                            parametros: 'Amocali',
+                            titulo: "Alerta de recolección",
+                            cuerpo: `La alerta ${alerta.IdAlerta} subió a nivel Rojo`,
+                            url: "https://campolimpiojal.com/Alertas/alertaGeneradasDist.php"
+                        })
+                            .then(response => {
+                                console.log(response.data);
+                            })
+                            .catch(error => { console.log(error); });
+                    } //Fin del if
+
+                });//Fin de la funcion actualiza
+            }
+
+        });
+
+    });
+
+
+});
+
+Tareas.comprobarAlerta().then(datos => {
+    console.log(datos.NoAlertas);
+    if (datos.NoAlertas > 0)
+        task.start();
+
+})
 
 // Settings
 app.set("port", config.port);
