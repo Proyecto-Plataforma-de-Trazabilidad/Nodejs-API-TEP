7d6e872ca12cf8e97b8a6dec4c9be21bde871635
SergioPiña <117691802+InfectedCupcake@users.noreply.github.com>
2023-10-25 00:58:12 -0600

    Habilitando cors

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { methods as usuariosController } from "../controllers/usuarios.controller.js";
+import cors from "cors";
 
 const router = Router();
 
-router.get("/", usuariosController.getUsuarios);
-router.post("/", usuariosController.agregarUsuario);
-router.post("/validar", usuariosController.validarUsuario);
-router.post("/primarioId", usuariosController.getUsuarioPXId);
+var corsOptions = {
+    origin: 'https://sacnej.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.get("/", cors(corsOptions), usuariosController.getUsuarios);
+router.post("/", cors(corsOptions), usuariosController.agregarUsuario);
+router.post("/validar", cors(corsOptions), usuariosController.validarUsuario);
+router.post("/primarioId", cors(corsOptions), usuariosController.getUsuarioPXId);
 export default router;
